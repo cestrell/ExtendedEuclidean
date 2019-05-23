264332b51e30ddafbe89f10bb217e742c0ed8dc9
Carlos Estrella <cestrell@umich.edu>
2019-05-23 07:30:02 -0400

    Code refactoring

--- extended_euclid.cpp
+++ extended_euclid.cpp
@@ -18,22 +18,20 @@ void generateRandomNumbers(mpz_t &num1, mpz_t &num2) {
 	mpz_urandomb(num1, state, 4096);
 	mpz_urandomb(num2, state, 4096);
 	
-	gmp_printf("\nRandom number 1:\n%Zd\n", num1);
-	gmp_printf("\nRandom number 2:\n%Zd\n", num2);
+	gmp_printf("Random number 1:\n%Zd\n", num1);
+	gmp_printf("\nRandom number 2:\n%Zd\n\n", num2);
 	gmp_randclear(state);
 }
 
 void computeGCD(mpz_t &num1, mpz_t&num2, mpz_t &bezout_x, mpz_t &bezout_y) {
-	mpz_t x, y;
-	mpz_init(x);
+	mpz_t x, y, zero;
+	mpz_inits(x, zero, NULL);
 	mpz_init_set_ui(y, 1);
 
-	mpz_t zero;
-	mpz_init(zero);
-
+	// Extended Euclidean Algorithm
 	while (mpz_cmp(num2, zero) != 0) {
-		mpz_t quotient, remainder;
-		mpz_inits(quotient, remainder, NULL);
+		mpz_t quotient, remainder, placeholder;
+		mpz_inits(quotient, remainder, placeholder, NULL);
 
 		mpz_tdiv_q(quotient, num1, num2);
 		mpz_tdiv_r(remainder, num1, num2);
@@ -41,24 +39,21 @@ void computeGCD(mpz_t &num1, mpz_t&num2, mpz_t &bezout_x, mpz_t &bezout_y) {
 		mpz_set(num1, num2);
 		mpz_set(num2, remainder);
 
-		mpz_t temp;
-		mpz_init(temp);
-
-		mpz_t temp_x;
+		mpz_t temp_x, temp_y;
 		mpz_init_set(temp_x, x);
-		mpz_mul(temp, quotient, x);
-		mpz_sub(temp, bezout_x, temp);
-		mpz_set(x, temp);
+		mpz_init_set(temp_y, y);
+
+		mpz_mul(placeholder, quotient, x);
+		mpz_sub(placeholder, bezout_x, placeholder);
+		mpz_set(x, placeholder);
 		mpz_set(bezout_x, temp_x);
 
-		mpz_t temp_y;
-		mpz_init_set(temp_y, y);
-		mpz_mul(temp, quotient, y);
-		mpz_sub(temp, bezout_y, temp);
-		mpz_set(y, temp);
+		mpz_mul(placeholder, quotient, y);
+		mpz_sub(placeholder, bezout_y, placeholder);
+		mpz_set(y, placeholder);
 		mpz_set(bezout_y, temp_y);
 
-		mpz_clears(quotient, remainder, temp, temp_x, temp_y, NULL);
+		mpz_clears(quotient, remainder, placeholder, temp_x, temp_y, NULL);
 	}
 	mpz_clears(x, y, zero, NULL);
 }
@@ -91,32 +86,19 @@ int main(int argc, char *argv[]) {
 	mpz_t gmp_gcd_res;
 	mpz_init(gmp_gcd_res);
 	mpz_gcd(gmp_gcd_res, num1, num2);
-	gmp_printf("\nGMP GCD:\n%Zd\n", gmp_gcd_res);
+	gmp_printf("GMP GCD:\n%Zd\n", gmp_gcd_res);
 	mpz_clear(gmp_gcd_res);
 
 	// Compute GCD using extended euclidean algorithm implementation
-	short bez1 = 1;
-	short bez2 = 2;
-	if (mpz_cmp(num1, num2) < 0) {
-		mpz_swap(num1, num2);
-		swap(bez1, bez2);
-	}
-
 	mpz_t bezout_x, bezout_y;
 	mpz_init_set_ui(bezout_x, 1);
 	mpz_init(bezout_y);
-
 	computeGCD(num1, num2, bezout_x, bezout_y);
 	
 	// Print computed results
 	gmp_printf("\nComputed GCD:\n%Zd\n", num1);
-
-	cout << "\nBezout Coefficient for number " << bez1 << ":\n";
-	mpz_out_str(stdout, 10, bezout_x);
-
-	cout << "\n\nBezout Coefficient for number " << bez2 << ":\n";
-	mpz_out_str(stdout, 10, bezout_y);
-	cout << '\n';
+	gmp_printf("\nBezout Coefficient for number 1:\n%Zd\n", bezout_x);
+	gmp_printf("\nBezout Coefficient for number 2:\n%Zd\n\n", bezout_y);
 
 	// Clear memory locations
 	mpz_clears(num1, num2, bezout_x, bezout_y, NULL);
